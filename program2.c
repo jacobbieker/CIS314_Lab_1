d5ca6594d0d31226121dfd71646e928336707596
Jacob Bieker <jacob@bieker.tech>
2015-10-12 13:38:50 -0700

    Add iterative approach
    Converted from the Java example here:
    http://www.cs.utsa.edu/~wagner/CS3343/recursion/binsearch.html

--- program2.c
+++ program2.c
@@ -44,5 +44,22 @@ int recursiveBinarySearch(int list_of_numbers[], int desired_number, int low_num
 
 int iterativeBinarySearch(int list_of_numbers[], int desired_number) {
 	int low = 0;
-	int high = (sizeof(list_of_numbers)/sizeof(list_of_numbers[0])) - 1
+	int high = (sizeof(list_of_numbers) / sizeof(list_of_numbers[0])) - 1;
+
+	while (low <= high)
+	{
+		int mid = (high + low) / 2;
+		if (mid == desired_number)
+		{
+			return mid;
+		}
+		else if (list_of_numbers[mid] < desired_number) {
+			low = mid + 1;
+		}
+		else {
+			high = mid - 1;
+		}
+	}
+
+	return -1;
 }
